c64cc36f5af495b7360ee92a6aeae954c18075ef
Jaggmort <djeedo@gmail.com>
2023-05-20 12:59:42 +0300

    Create HTML page with books.

---
+++ render_website.py
@@ -0,0 +1,38 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import warnings
+import os
+from dotenv import load_dotenv
+import json
+
+
+def main():
+    load_dotenv()
+    warnings.filterwarnings("ignore")
+    json_path = os.environ.get('JSON_PATH')
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    with open(json_path, 'r', encoding='utf8') as file:
+        books = json.load(file)
+    print(books[0].keys())
+    for book in books:
+        if not os.path.isfile(book['img_src']):
+            book['img_src'] = 'no_file'
+    rendered_page = template.render(
+        books=books,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
